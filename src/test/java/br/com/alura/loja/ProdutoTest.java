efe4c63effeeaa0c080bbfc1621499d2c8cbd3ae
Wagner Galvao <galvao.wagner@outlook.com.br>
2021-06-24 12:14:36 -0300

    Atualizado em 24/06/2021 12:14:19

--- src/test/java/br/com/alura/loja/ProdutoTest.java
+++ src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -1,14 +1,20 @@
 package br.com.alura.loja;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
 
 import org.junit.Test;
 
 public class ProdutoTest {
 
+	private Produto produto;
+
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		produto = new Produto("teste", BigDecimal.TEN);
+		assertEquals("teste", produto.getNome());
+		assertEquals(BigDecimal.TEN, produto.getPreco());
 	}
 
 }
